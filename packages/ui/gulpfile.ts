ae0b0aa2b320db10c1e78eb907d82ef7f4de1998
戴鹏鹏 <16602124116@163.com>
2022-06-26 15:49:33 +0800

    feat: 项目搭建

--- packages/ui/gulpfile.ts
+++ packages/ui/gulpfile.ts
@@ -3,17 +3,18 @@ const less = require('gulp-less')
 const { resolve } = require('path')
 
 const { series, parallel, src, dest } = require('gulp')
+const cleanCSS = require('gulp-clean-css')
 
-function buildESLess() {
-  return src(resolve(__dirname, 'src/style/**/*.less'))
-    .pipe(less())
-    .pipe(dest('./es/theme'))
+function buildCss(target: 'esm' | 'cjs') {
+  let output = ''
+  if (target === 'esm') output = 'es'
+  if (target === 'cjs') output = 'lib'
+  return () => {
+    return src(resolve(__dirname, 'src/style/**/*.less'))
+      .pipe(less())
+      .pipe(cleanCSS())
+      .pipe(dest(`${output}/theme`))
+  }
 }
 
-function buildLibLess() {
-  return src(resolve(__dirname, 'src/style/**/*.less'))
-    .pipe(less())
-    .pipe(dest('./lib/theme'))
-}
-
-exports.default = series(parallel(buildESLess, buildLibLess))
+exports.default = series(parallel(buildCss('esm'), buildCss('cjs')))
