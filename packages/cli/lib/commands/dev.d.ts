21305d3d8bd9d38a4aa59f690eba3483b099eebd
戴鹏鹏 <16602124116@163.com>
2022-06-27 14:04:15 +0800

    feat: 项目搭建

--- packages/cli/lib/commands/dev.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function dev(cmd: {
-    simple: boolean;
-}): Promise<void>;
--- packages/cli/lib/utils/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const replaceExt: (file: string, ext: string) => string;
-export declare function smallCamel(str: string, separator?: '-' | '_'): string;
-export declare function bigCamel(str: string, separator?: '-' | '_'): string;
-export declare const isDir: (file: string) => boolean;
-export declare const isFileType: (file: string, ext: 'less' | 'ts' | 'vue') => boolean;
-export declare const isSFC: (file: string) => boolean;
-export declare const isTS: (file: string) => boolean;
-export declare const isLess: (file: string) => boolean;
-export declare function smartAppendFileSync(file: string, code: string): void;
